bf0033026fb582723ddcd46599633871f1b9f2aa
贺昱皓 <heyuhao@uino.com>
2022-08-17 15:31:52 +0800

    test: changelog

--- packages/cli-a/src/cli.ts
+++ packages/cli-a/src/cli.ts
@@ -2,7 +2,7 @@ import utils from '@bl-pnpm/cli-utils'
 import log from '@bl-pnpm/cli-log'
 
 
-log.verbose('1 + 2 =', utils.math.add(1, 2) + '')
+log.verbose('1 + 2 = ', utils.math.add(1, 2).toString())
 
 export default {
   name: 'cli-a'
