a421baa88a58232f6d7dfe4ce26558448101d5ff
贺昱皓 <heyuhao@uino.com>
2022-08-17 14:10:58 +0800

    release: cli-a@1.0.7

--- packages/cli-a/src/cli.ts
+++ packages/cli-a/src/cli.ts
@@ -2,7 +2,7 @@ import utils from '@banli17/template-cli-pnpm-utils'
 import log from '@banli17/template-cli-pnpm-log'
 
 
-log.verbose('1 + 2= ', utils.math.add(1, 2) + '')
+log.verbose('1 + 2 = ', utils.math.add(1, 2) + '')
 
 export default {
   name: 'cli-a'
